d7864050a9120080be64f10a809d7bd0943c1548
evgueniazet <zelenkoea19@gmail.com>
2023-03-22 19:49:21 +0100

    fix: change webpack config

--- shelter/webpack.build.config.js
+++ shelter/webpack.build.config.js
@@ -1,12 +1,18 @@
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ExtractCssChunksPlugin = require('extract-css-chunks-webpack-plugin');
 
 module.exports = {
-  entry: __dirname + "/src/js/index.js",
+  entry: {
+    main: path.resolve(__dirname, "/src/js/index.js"),
+    pets: path.resolve(__dirname, "/src/js/pets.js"),
+  },
   output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js',
-    publicPath: '/'
+    path: path.resolve(__dirname, "dist"),
+    filename: '[name].js',
+    clean: true,
   },
   mode: 'production',
   module: {
@@ -14,7 +20,7 @@ module.exports = {
       {
         test: /\.(s[ac]|c)ss$/i,
         use: [
-          'style-loader',
+          MiniCssExtractPlugin.loader,
           'css-loader',
           'resolve-url-loader',
           { loader: 'sass-loader', options: { sourceMap: true } },
@@ -23,20 +29,41 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      }
+        generator: {
+          filename: "./images/[hash][ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "./fonts/[name][ext]",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
     ]
   },
   plugins: [
+
     new HtmlWebpackPlugin({
-      template: __dirname + "/src/index.html",
-      inject: 'body'
+      template: path.resolve(__dirname, "/src/index.html"),
+      inject: 'body',
+      chunks: ['main']
     }),
-    new MiniCssExtractPlugin()
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "/src/pets.html"),
+      inject: 'body',
+      filename: 'pets.html',
+      chunks: ['pets'],
+    }),
+
+    new MiniCssExtractPlugin(),
   ],
-  devServer: {
-    static: './',
-    port: 3000,
-    open: true,
-    hot: true,
-  },
 };
--- shelter/webpack.dev.config.js
+++ shelter/webpack.dev.config.js
@@ -2,10 +2,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: __dirname + "/src/js/index.js",
+  entry: {
+    main: path.resolve(__dirname, "/src/js/index.js"),
+    pets: path.resolve(__dirname, "/src/js/pets.js"),
+  },
+
   output: {
     path: __dirname + '/dist',
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     publicPath: '/'
   },
   mode: 'development',
@@ -23,14 +27,29 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
-      }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+
+          },
+        ],
+      },
     ]
   },
   plugins: [
+
     new HtmlWebpackPlugin({
       template: __dirname + "/src/index.html",
-      inject: 'body'
-    })
+      chunks: ['main']
+    }),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/pets.html",
+      filename: 'pets.html',
+      chunks: ['pets'],
+    }),
   ],
   devServer: {
     static: './',
