70365cddb60f6f36783c6a952d29b03e167bb3e8
evgueniazet <zelenkoea19@gmail.com>
2023-04-10 20:14:53 +0200

    feat: add pagination on main page

--- shelter/src/js/features/pagination.js
+++ shelter/src/js/features/pagination.js
@@ -2,13 +2,26 @@ import { createCard } from "../utils/createCard";
 import { shuffleArray } from "../utils/shuffleArray";
 import { pets } from "../data";
 
-
 export const pagination = () => {
     const cardsContainer = document.querySelector('.cards-container');
     const cardsButton = document.querySelectorAll('.cards-button');
     let count = 1;
+    let petsArr = [];
+
+    if (count === 1) {
+        cardsButton[0].classList.add('cards-button-inactive');
+        cardsButton[1].classList.add('cards-button-inactive');
+    }
 
     const initialWidth = document.body.offsetWidth;
+
+    const handleResize = () => {
+        const width = document.body.offsetWidth;
+        getPagesQuantity(width);
+        getPetsOnPageQuantity(width);
+        console.log('pagesQuantity', pagesQuantity);
+        cardsButton[2].innerText = pagesQuantity;
+    };
 
     const getPagesQuantity = (width) => {
         if (width > 1075) {
@@ -20,86 +33,132 @@ export const pagination = () => {
         }
     };
 
-    // const getPetsOnPageQuantity = (width) => {
-    //     if (width > 1075) {
-    //         return 8;
-    //     } else if (width > 595 && width <= 1075) {
-    //         return 6;
-    //     } else if (width <= 595) {
-    //         return 3;
-    //     }
-    // };
-
-    // const petsOnPage = getPetsOnPageQuantity(initialWidth);
-
-    // console.log('petsOnPage', petsOnPage);
+    const getPetsOnPageQuantity = (width) => {
+        if (width > 1075) {
+            return 8;
+        } else if (width > 595 && width <= 1075) {
+            return 6;
+        } else if (width <= 595) {
+            return 3;
+        }
+    };
 
     const pagesQuantity = getPagesQuantity(initialWidth);
+    const petsOnPageQuantity = getPetsOnPageQuantity(initialWidth);
 
+    for (let i = 0; i < 6; i++) {
 
+        shuffleArray(pets);
 
-    // const createPetsArray = () => {
-    //     let petsCopy = [...pets];
-    //     for (let i = 0; i < 6; i++) {
-    //         shuffleArray(petsCopy);
-    //         console.log('petsCopy', petsCopy);
-    //     }
-    // };
-    // const petsArray = createPetsArray();
-    // console.log('petsArray', petsArray);
+        const sliceArr = (arr, size) => {
+            const res = [];
+            for (let i = 0; i < arr.length; i += size) {
+                const chunk = arr.slice(i, i + size);
+                res.push(chunk);
+            }
+            return res;
+        };
 
-    shuffleArray(pets);
+        const slicedArray = sliceArr(pets, 3);
 
-    const sliceArr = (arr, size) => {
-        const res = [];
-        for (let i = 0; i < arr.length; i += size) {
-            const chunk = arr.slice(i, i + size);
-            res.push(chunk);
-        }
-        return res;
+        slicedArray.forEach((array) => {
+            shuffleArray(array);
+            array.forEach((elem) => {
+                petsArr.push(elem);
+            })
+        })
+    }
+
+    const drawInitialCards = () => {
+        const initialArr = petsArr.slice(0, petsOnPageQuantity);
+        initialArr.forEach((item) => cardsContainer.appendChild(createCard(item)));
     };
 
-    const slicedArray = sliceArr(pets, 3);
-    console.log('slicedArray', slicedArray);
+    drawInitialCards();
 
-
-    const shuffleSliceArr = (arr) => {
-        arr.forEach((item) => { shuffleArray(item) })
+    const drawCards = (quantity) => {
+        const number = cardsButton[2].innerText;
+        const startIndex = (number - 1) * quantity;
+        const endIndex = number * quantity;
+        const drawPets = petsArr.slice(startIndex, endIndex);
+        drawPets.forEach((item) => cardsContainer.appendChild(createCard(item)));
     };
 
-    shuffleSliceArr(slicedArray);
-
-    console.log('shuffledSliceArr', slicedArray);
-
-
-
-    pets.forEach((item) => cardsContainer.appendChild(createCard(item)));
+    const drawLastCards = (quantity) => {
+        const number = cardsButton[2].innerText;
+        const startIndex = petsArr.length - quantity;
+        const endIndex = number * quantity;
+        const drawPets = petsArr.slice(startIndex, endIndex);
+        drawPets.forEach((item) => cardsContainer.appendChild(createCard(item)));
+    };
 
     cardsButton[2].innerText = count;
+
+    const activateLeftButtons = () => {
+        cardsButton[0].classList.remove('cards-button-inactive');
+        cardsButton[1].classList.remove('cards-button-inactive');
+    };
+
+    const activateRightButtons = () => {
+        cardsButton[3].classList.remove('cards-button-inactive');
+        cardsButton[4].classList.remove('cards-button-inactive');
+    };
+
+    const inactivateLeftButtons = () => {
+        cardsButton[0].classList.add('cards-button-inactive');
+        cardsButton[1].classList.add('cards-button-inactive');
+    };
+
+    const inactivateRightButtons = () => {
+        cardsButton[3].classList.add('cards-button-inactive');
+        cardsButton[4].classList.add('cards-button-inactive');
+    };
 
     cardsButton[0].addEventListener('click', () => {
         cardsButton[2].innerText = 1;
         count = 1;
+        cardsContainer.querySelectorAll('.card').forEach((item) => item.parentNode.removeChild(item));
+        drawInitialCards();
+        inactivateLeftButtons();
+        activateRightButtons();
     })
 
     cardsButton[1].addEventListener('click', () => {
         if (cardsButton[2].innerText > 1) {
             count = count - 1;
             cardsButton[2].innerText = count;
-        } else {
+            cardsContainer.querySelectorAll('.card').forEach((item) => item.parentNode.removeChild(item));
+            drawCards(petsOnPageQuantity);
+        } else if (cardsButton[2].innerText == 1) {
+            inactivateLeftButtons();
+            activateRightButtons();
         }
     })
 
     cardsButton[3].addEventListener('click', () => {
         if (cardsButton[2].innerText < pagesQuantity) {
             count++;
+            activateLeftButtons();
             cardsButton[2].innerText = count;
-        } else {
+            cardsContainer.querySelectorAll('.card').forEach((item) => item.parentNode.removeChild(item));
+            drawCards(petsOnPageQuantity);
+        } else if (cardsButton[2].innerText == pagesQuantity) {
+            activateLeftButtons()
+            inactivateRightButtons();
+            cardsButton[3].disabled = true;
+            cardsButton[4].disabled = true;
         }
     })
 
     cardsButton[4].addEventListener('click', () => {
         cardsButton[2].innerText = pagesQuantity;
         count = pagesQuantity;
+        cardsContainer.querySelectorAll('.card').forEach((item) => item.parentNode.removeChild(item));
+        drawLastCards(petsOnPageQuantity);
+        activateLeftButtons();
+        inactivateRightButtons();
     })
+
+    window.addEventListener('resize', handleResize);
+
 };
